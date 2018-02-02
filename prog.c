318d37059c78145e50ed0ab3ba67c618c6173987
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-02 13:40:45 +0530

    prog.c

---
+++ prog.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char b;
+printf("enter the letter");
+scanf("%c",&b);
+if(b=='a'!!b=='e'!!c=='i'!! b=='o'!!b=='u');
+printf("it is the vowel letter");
+else
+printf("it is not the vowel letter");
+getch();
+return 0;
+}
