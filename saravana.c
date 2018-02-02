5675f52c2b7aca6283fbbc1c4d5509e741301ed9
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-02 13:26:40 +0530

    saravana.c

---
+++ saravana.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int b;
+printf("enter number b");
+scanf("%d",&b);
+if(b/2==0)
+printf("the number is even");
+else
+printf("the number is odd");
+return 0;
+getch ()
+}
