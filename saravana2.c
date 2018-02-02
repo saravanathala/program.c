19fc2c4a5a95ff454054f7f041531bd9eac3ab47
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-02-02 13:58:12 +0530

    saravana2.c

---
+++ saravana2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a%4==0)
+{
+printf("the year is %d leap year ");
+}
+else
+{
+printf("the year is not a leap year");
+}
+return o;
+}
