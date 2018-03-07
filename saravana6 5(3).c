e43045faba7151c335e3af9d332d6511b7a76f4f
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-07 16:24:06 +0530

    saravana6 5(3).c

---
+++ saravana6 5(3).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int ,a,b;
+printf("enter the number");
+scanf("%d",&a);
+for (i=0;i<=5;i++);
+printf("the enter the value %d ");
+scanf(""%D,&b);
+getch();
+return 0;
+}
