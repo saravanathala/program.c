c59a46f5f451ac5f328404958f76e0ce7b9fc822
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-07 15:46:02 +0530

    saravana6 2(3).c

---
+++ saravana6 2(3).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int num;
+ printf("enter the number:");
+ scanf("%d",&num);
+ if(num%2==0)
+ {
+   printf("%d",num-2);
+   }
+   else
+   {
+    printf("%d",num-1);
+    }
+    getch();
+    return 0;
+    }
