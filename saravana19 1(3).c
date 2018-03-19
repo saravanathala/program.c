a9f45abbf8809f447b985ea1ed11f7308e371a17
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-19 03:14:25 -0700

    saravana19 1(3).c

---
+++ saravana19 1(3).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter the value\n");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+return 0;
+}
