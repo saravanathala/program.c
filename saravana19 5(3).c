1f3378bd170bc6a683c45ec6a1ec571686f73fb1
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-19 03:57:45 -0700

    saravana19 5(3).c

---
+++ saravana19 5(3).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+
+    float l,b,a;
+    printf("enter b and l");
+    scanf("%f %f",&l,&b);
+    a=l*b;
+    printf("area of farm is=%.5f",a);
+    scanf ("enter the &a);
+    getch();
+    return 0;
+    }
