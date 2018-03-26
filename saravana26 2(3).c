13b2792323486eb0ec2fda83ee8ddc6114e6b03d
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-26 15:53:38 +0530

    saravana26 2(3).c

---
+++ saravana26 2(3).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,sum=0,i;
+	scanf("%d",&a);
+	for(i=0;i<a;i++)
+	{
+		scanf("%d",&b);
+		sum=sum+b;
+	}
+	printf("%d",sum);
+  scanf("%d",d);
+  getch();
+  return 0;
+  }
