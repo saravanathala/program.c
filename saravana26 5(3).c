daf2fbe739c38b23e9636e10ba6037506832128a
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-26 16:07:39 +0530

    saravana26 5(3).c

---
+++ saravana26 5(3).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int p,t,r,i;
+printf("\n enter the p,t,r");
+scanf("%d\t%d\t%d",&p,&t,&r);
+i=p*t*r/100;
+printf("\n interest=%d",i);
+getch();
+return 0;
+}
