33720596c2388067b711eacd7f523c70fb2a3cba
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-26 16:02:11 +0530

    saravana26 4(3).c

---
+++ saravana26 4(3).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int b,c,a[100],i;
+printf("Enter  the b & c:");
+scanf("%d%d",&b,&c);
+printf("\nEnter the n numbers:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("\n The c number is %d",a[kc-1]);
+getch();
+}
