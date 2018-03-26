711c2ba581afa3cf964306ec13600d177908c1a9
saravanathala <35689043+saravanathala@users.noreply.github.com>
2018-03-26 15:58:27 +0530

    saravana26 3(3).c

---
+++ saravana26 3(3).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  public static void main(String aa[]){
+    int s,b,c;
+    Scanner s=new Scanner(System.in);
+    s=s.nextInt();
+    double n=as+273.5;
+    System.out.println(n);
+
+           
+   }
+   getch();
+   return 0;
+}
